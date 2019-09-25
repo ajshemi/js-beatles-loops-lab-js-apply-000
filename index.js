206c45e0e90f9ce18e4df3b2e15452de76adc25a
essential-hacker-4489 <akpojotor@yahoo.com>
2019-09-25 22:24:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var theBeatlesPlay=function(array1,array2){
   var emptyArray=[]
   for(var i=0;i<array1.length;i++){
-    var str=`${array1[i]} plays ${array2[i}`
+    var str=`${array1[i]} plays ${array2[i]}`
     emptyArray.push(str)
   }
   return emptyArray
